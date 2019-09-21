6c375f210ad11a2ca09ede2e28b12333946fe2be
leecw05 <leecw05@naver.com>
2019-09-22 01:17:41 +0900

    Fisrt Test
    First TEst

---
+++ Assets/TestRoteateImage.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRoteateImage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float rotSpeed;
+
+    void Start()
+    {
+        Debug.Log("Hello, Wrold");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)) {
+            rotSpeed = 10;
+        }
+        transform.Rotate(0, 0, this.rotSpeed);  
+        rotSpeed *= 0.96f;  
+    }
+}
